ac6e5d5a80741037f476c05bfded2d73de3641b7
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-07-10 10:26:40 -0400

    clean up lexical_cast unit test

--- cpp/lexical_cast.cpp
+++ cpp/lexical_cast.cpp
@@ -3,8 +3,13 @@
 //
 //   template<typename T> T lexical_cast(const std::string &x);
 //
-// which converts a string to type T.
-// Currently only T=int, T=long, T=double, and T=float are implemented (plus trivial case T=string)
+// which converts a string to type T.  Currently the following types T are supported:
+//
+//   string  (trivial)
+//   long
+//   int
+//   double
+//   uint16_t
 //
 // Note that a more general implementation of lexical_cast is already defined in boost, and considered 
 // for inclusion in C++ TR2, but it's not in C++11, so we're forced to define it here!
@@ -108,64 +113,33 @@ template<> float lexical_cast<float> (const string &x)
 }
 
 
-static void check_convert_int(const string &x, int y)
+// -------------------------------------------------------------------------------------------------
+//
+// Unit test
+
+
+template<typename T> 
+static void check_convert(const string &x, T y)
 {
-    int ret;
+    T ret;
 
     try {
-	ret = lexical_cast<int> (x);
+	ret = lexical_cast<T> (x);
     }
     catch (...) {
 	cerr << "test_lexical_cast(): threw unexpected exception\n";
 	exit(1);
     }
 
-    if (ret != y) {
+    if (fabs(double(ret) - double(y)) > 1.0e-5) {
 	cerr << "test_lexical_cast(): didn't correctly convert\n";
 	exit(1);
     }
 }
 
 
-static void check_convert_double(const string &x, double y)
-{
-    double ret;
-
-    try {
-	ret = lexical_cast<double> (x);
-    }
-    catch (...) {
-	cerr << "test_lexical_cast(): threw unexpected exception\n";
-	exit(1);
-    }
-
-    if (fabs(ret-y) > 1.0e-15) {
-	cerr << "test_lexical_cast(): didn't correctly convert\n";
-	exit(1);
-    }
-}
-
-
-static void check_convert_uint16_t(const string &x, uint16_t y)
-{
-    uint16_t ret;
-
-    try {
-	ret = lexical_cast<uint16_t> (x);
-    }
-    catch (...) {
-	cerr << "test_lexical_cast(): threw unexpected exception\n";
-	exit(1);
-    }
-
-    if (ret != y) {
-	cerr << "test_lexical_cast(): didn't correctly convert\n";
-	exit(1);
-    }
-}
-
-
-template<typename T> static void check_convert_throws(const string &x)
+template<typename T> 
+static void check_convert_throws(const string &x)
 {
     try {
 	lexical_cast<T> (x);
@@ -181,11 +155,11 @@ template<typename T> static void check_convert_throws(const string &x)
 
 void test_lexical_cast()
 {
-    check_convert_int("0", 0);
-    check_convert_int("-0", 0);
-    check_convert_int("12", 12);
-    check_convert_int("-123", -123);
-    check_convert_int(" \t 1234  \n\t", 1234);
+    check_convert<int>("0", 0);
+    check_convert<int>("-0", 0);
+    check_convert<int>("12", 12);
+    check_convert<int>("-123", -123);
+    check_convert<int>(" \t 1234  \n\t", 1234);
 
     check_convert_throws<int>("");
     check_convert_throws<int>("  ");
@@ -195,17 +169,17 @@ void test_lexical_cast()
     check_convert_throws<int>("1234 abc");
     check_convert_throws<int>("0.1");
 
-    check_convert_uint16_t("0", 0);
-    check_convert_uint16_t("65535", 65535);
+    check_convert<uint16_t>("0", 0);
+    check_convert<uint16_t> ("65535", 65535);
     check_convert_throws<uint16_t> ("-1");
     check_convert_throws<uint16_t> ("65536");
 
-    check_convert_double("1.23", 1.23);
-    check_convert_double("-1.23e-5", -1.23e-5);
-    check_convert_double("-5", -5.0);
-    check_convert_double(".23", 0.23);
-    check_convert_double("-.034e3", -0.034e3);
-    check_convert_double("  0.03e20  ", 0.03e20);
+    check_convert<double>("1.23", 1.23);
+    check_convert<double>("-1.23e-5", -1.23e-5);
+    check_convert<double>("-5", -5.0);
+    check_convert<double>(".23", 0.23);
+    check_convert<double>("-.034e3", -0.034e3);
+    check_convert<double>("  0.03e20  ", 0.03e20);
 
     check_convert_throws<double>("");
     check_convert_throws<double>("  ");
