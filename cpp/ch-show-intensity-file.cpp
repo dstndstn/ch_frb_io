bcc1179d267fddbfef4eb1c3f3257d81049f85c3
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-09-09 12:28:34 -0400

    add -m option to ch-show-intensity-file

--- cpp/ch-show-intensity-file.cpp
+++ cpp/ch-show-intensity-file.cpp
@@ -8,8 +8,9 @@ using namespace ch_frb_io;
 
 static void usage()
 {
-    cerr << "usage: ch-show-intensity-file [-t] <filename1.h5> <filename2.h5> ...\n"
-	 << "       if the -t flag is specified, then unit tests will be run\n";
+    cerr << "usage: ch-show-intensity-file [-t] [-m] <filename1.h5> <filename2.h5> ...\n"
+	 << "       if -t is specified, then unit tests will be run\n"
+	 << "       if -m is specified, then the mean intensity/weight will be shown in each channel (generates a lot of output!)\n";
 
     exit(2);
 }
@@ -19,12 +20,15 @@ int main(int argc, char **argv)
 {
     vector<string> filename_list;
     bool run_unit_tests = false;
+    bool show_mean_values = false;
 
     // Low-budget command line parsing
 
     for (int i = 1; i < argc; i++) {
         if (!strcmp(argv[i], "-t"))
 	    run_unit_tests = true;
+        else if (!strcmp(argv[i], "-m"))
+	    show_mean_values = true;
 	else
 	    filename_list.push_back(string(argv[i]));
     }
@@ -32,13 +36,38 @@ int main(int argc, char **argv)
     if (filename_list.size() == 0)
 	usage();
 
-    for (unsigned int i = 0; i < filename_list.size(); i++) {
-	if (i > 1)
+    for (unsigned int ifile = 0; ifile < filename_list.size(); ifile++) {
+	if (ifile > 1)
 	    cout << "\n";
 
-	bool noisy = true;
-	intensity_hdf5_file f(filename_list[i], noisy);
+	intensity_hdf5_file f(filename_list[ifile], true);  // noisy=true
     
+	if (show_mean_values) {
+	    int ns = f.npol * f.nt_file;
+
+	    if (ns == 0)
+		throw runtime_error("No data in file!\n");
+
+	    for (int ifreq = 0; ifreq < f.nfreq; ifreq++) {
+		double sum_wi = 0.0;
+		double sum_wt = 0.0;
+
+		for (int s = 0; s < ns; s++) {
+		    sum_wi += f.weights[ifreq*ns+s] * f.intensity[ifreq*ns+s];
+		    sum_wt += f.weights[ifreq*ns+s];
+		}
+
+		cout << "channel " << ifreq << "/" << f.nfreq << ": ";
+		
+		if (sum_wt <= 0.0) {
+		    cout << "empty\n";
+		    continue;
+		}
+
+		cout << " mean_int=" << (sum_wi/sum_wt) << ", mean_wt=" << (sum_wt/ns) << endl;
+	    }
+	}
+
 	cout << "    nfreq = " << f.nfreq << endl
 	     << "    npol = " << f.npol << endl
 	     << "    nt_file = " << f.nt_file << "                  # Number of time indices in file\n"
